a117060f8d5d6c3d8cd59f7fbed86f155b7a61ad
陈建祥 <853409335@qq.com>
2025-02-14 11:24:41 +0800

    feaq: 报告保存本机逻辑

--- koom-demo/src/main/java/com/kwai/koom/demo/javaleak/OOMMonitorInitTask.kt
+++ koom-demo/src/main/java/com/kwai/koom/demo/javaleak/OOMMonitorInitTask.kt
@@ -8,10 +8,13 @@ import com.kwai.koom.javaoom.monitor.OOMHprofUploader
 import com.kwai.koom.javaoom.monitor.OOMMonitorConfig
 import com.kwai.koom.javaoom.monitor.OOMReportUploader
 import java.io.File
+import java.io.IOException
 
 object OOMMonitorInitTask : InitTask {
 
   override fun init(application: Application) {
+    val appFilesDir = application.filesDir // 获取 app 的 files 目录
+
     val config = OOMMonitorConfig.Builder()
       .setThreadThreshold(50) //50 only for test! Please use default value!
       .setFdThreshold(300) // 300 only for test! Please use default value!
@@ -22,15 +25,25 @@ object OOMMonitorInitTask : InitTask {
       .setAnalysisPeriodPerVersion(15 * 24 * 60 * 60 * 1000) // Consider use default value！
       .setLoopInterval(5_000) // 5_000 for test! Please use default value!
       .setEnableHprofDumpAnalysis(true)
+      // 存储 HPROF 文件
       .setHprofUploader(object : OOMHprofUploader {
         override fun upload(file: File, type: OOMHprofUploader.HprofType) {
-          MonitorLog.e("OOMMonitor", "todo, upload hprof ${file.name} if necessary")
+          val destFile = File(appFilesDir, file.name) // 复制到 app files 目录
+          file.copyTo(destFile, overwrite = true)
+          MonitorLog.e("OOMMonitor", "HPROF 文件已保存: ${destFile.absolutePath}")
         }
       })
+
+      // 存储 JSON 报告
       .setReportUploader(object : OOMReportUploader {
         override fun upload(file: File, content: String) {
-          MonitorLog.i("OOMMonitor", content)
-          MonitorLog.e("OOMMonitor", "todo, upload report ${file.name} if necessary")
+          val destFile = File(appFilesDir, file.name) // 复制到 app files 目录
+          try {
+            destFile.writeText(content)
+            MonitorLog.i("OOMMonitor", "OOM JSON 报告已保存: ${destFile.absolutePath}")
+          } catch (e: IOException) {
+            MonitorLog.e("OOMMonitor", "写入 JSON 失败: ${e.message}")
+          }
         }
       })
       .build()
